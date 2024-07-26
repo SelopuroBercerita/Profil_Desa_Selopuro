b9ab7a703cce1a4048326cf40a1916232938700a
catashX <agustinuszefanya@gmail.com>
2024-07-26 12:03:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,16 @@ st.set_page_config(
 st.sidebar.success("Oleh tim KKN IPB 2024 Selopuro")
 st.title("home")
 
+with st.sidebar:
+        st.page_link('streamlit_app.py', label='home', icon='🏠')
+        st.page_link('pages/1_Gambaran_Umum_Desa.py', label='Gambaran Umum', icon='📕')
+        st.page_link('pages/2_Data_Sosial_Selopuro.py', label='Data Sosial', icon='🫂')
+        st.page_link('pages/3_Data_Ekonomi_Selopuro.py', label='Data Ekonomi', icon='💵')
+        st.page_link('pages/4_Data_Pemerintahan_Selopuro.py', label='Data Pemerintahan', icon='🏛️')
+        st.page_link('pages/5_Data_Demografis_Selopuro.py', label='Data Demografis', icon='🌏')
+
+
+
 title = """
 <div class="four">
   <h1> <span> Selopuro Bercerita </span> Profil Desa <em>Selopuro</em></h1>
