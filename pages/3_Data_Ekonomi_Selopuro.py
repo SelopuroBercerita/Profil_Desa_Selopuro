c4780d804c8d6b29e322a2c21040650e43883df0
catashX <agustinuszefanya@gmail.com>
2024-07-26 12:08:54 +0700

    asd

--- pages/3_Data_Ekonomi_Selopuro.py
+++ pages/3_Data_Ekonomi_Selopuro.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 with st.sidebar:
         st.page_link('streamlit_app.py', label='home', icon='🏠')
-        st.page_link('pages/1_Gambaran_Umum_Desa.py', label='Gambaran Umum', icon='📕')
+        st.page_link('pages/1_Gambaran_Umum_Desa_Selopuro.py', label='Gambaran Umum', icon='📕')
         st.page_link('pages/2_Data_Sosial_Selopuro.py', label='Data Sosial', icon='🫂')
         st.page_link('pages/3_Data_Ekonomi_Selopuro.py', label='Data Ekonomi', icon='💵')
         st.page_link('pages/4_Data_Pemerintahan_Selopuro.py', label='Data Pemerintahan', icon='🏛️')
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.sidebar.success("Oleh tim KKN IPB 2024 Selopuro")
 
 with st.sidebar:
         st.page_link('streamlit_app.py', label='home', icon='🏠')
-        st.page_link('pages/1_Gambaran_Umum_Desa.py', label='Gambaran Umum', icon='📕')
+        st.page_link('pages/1_Gambaran_Umum_Desa_Selopuro.py', label='Gambaran Umum', icon='📕')
         st.page_link('pages/2_Data_Sosial_Selopuro.py', label='Data Sosial', icon='🫂')
         st.page_link('pages/3_Data_Ekonomi_Selopuro.py', label='Data Ekonomi', icon='💵')
         st.page_link('pages/4_Data_Pemerintahan_Selopuro.py', label='Data Pemerintahan', icon='🏛️')
