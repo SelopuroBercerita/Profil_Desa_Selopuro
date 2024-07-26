9353ff834605b4f541d74d9068fed5683e51b2d2
catashX <agustinuszefanya@gmail.com>
2024-07-26 11:15:42 +0700

    Update 5_Data_Demografis_Selopuro.py

--- pages/5_Data_Demografis_Selopuro.py
+++ pages/5_Data_Demografis_Selopuro.py
@@ -7,3 +7,7 @@ st.set_page_config(
     page_icon = "Image/LogoSelopuroBercerita.png",
     page_title = "Selopuro Bercerita")
 
+df_lahan = pd.read_csv("Data\Demografis\Lahan.csv",sep=';')
+st.dataframe(df_lahan)
+chart = px.bar(df_lahan,x="Jenis Wilayah",y="Luas Wilayah (ha)")
+st.plotly_chart(chart)
